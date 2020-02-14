44829d371c39aa5547822d5604416fb1cfeefa9b
Smarth Bajaj <smarthbajaj28@gmail.com>
2020-02-14 21:10:52 +1100

    Commit

--- src/Page/SingleRoom.js
+++ src/Page/SingleRoom.js
@@ -1,7 +1,23 @@
 import React, { Component } from "react";
+import defaultBcg from "../images/room-1.jpeg";
+import Hero from "../Components/Hero";
+import Banner from "../Components/Banner";
+import { Link } from "react-router-dom";
+import { RoomContext } from "../context";
 
 export default class SingleRoom extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      slug: this.props.match.params.slug,
+      defaultBcg
+    };
+  }
+  static contextType = RoomContext;
+  //componentDidMount() {}
   render() {
+    const { getRoom } = this.context;
+    const room = getRoom(this.state.slug);
     return <div>hello from single room</div>;
   }
 }
